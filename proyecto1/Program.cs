ef213556260111d67bf3209b835160f93edd4886
usuario <usuario@LENOVO-PC>
2021-08-29 22:20:43 -0500

    Agregar archivos de proyecto.

---
+++ proyecto1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace proyecto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            int b = 12;
+            int c = 6;
+            int suma = a + b + c;
+            Console.WriteLine(" la suma es "  + suma);
+            _ = Console.ReadLine();
+        }
+    }
+}
